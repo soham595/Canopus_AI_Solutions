657f00895b57c49adde5991b59845203ecef545b
Soham Das <soham5959595@gmail.com>
2019-03-22 16:17:48 +0530

    Added requirements.txt Responsive Home Container Added images directory with sample image in static folder Added STATIC_ROOT in settings.py Need to test static folder image retrieval.

--- canopus/settings.py
+++ canopus/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = "/image/static/image/images"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "Canopus_AI_Solutions/canopus/media")
